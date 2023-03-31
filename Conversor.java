60e9edbb1a64a13c400355c79968786b2edef225
Philipp André Gysel <103286927+phisuico@users.noreply.github.com>
2023-03-31 20:01:55 -0300

    Add files via upload

--- Conversor.java
+++ Conversor.java
@@ -5,31 +5,31 @@ import javax.swing.JOptionPane;
 public class Conversor {
 
 	public void converterReaisParaDolares(double valorRecebido){
-        double moedaDolar = valorRecebido / 5.13;
+        double moedaDolar = valorRecebido / 5.09;
         moedaDolar = (double) Math.round(moedaDolar * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaDolar + " Dólares");
     }
 
     public void converterReaisParaEuros(double valorRecebido){
-        double moedaEuro = valorRecebido / 10.85;
+        double moedaEuro = valorRecebido / 5.55;
         moedaEuro = (double) Math.round(moedaEuro * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaEuro + " Euros");
     }
 
     public void converterReaisParaLibras(double valorRecebido){
-        double moedaLibra = valorRecebido / 6.33;
+        double moedaLibra = valorRecebido / 6.31;
         moedaLibra = (double) Math.round(moedaLibra * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaLibra + " Libras Esterlinas");
     }
 
     public void converterReaisParaPesosArgentinos(double valorRecebido){
-        double moedaPesoArgentino = valorRecebido / 0.039;
+        double moedaPesoArgentino = valorRecebido / 0.024;
         moedaPesoArgentino = (double) Math.round(moedaPesoArgentino * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaPesoArgentino + "Pesos Argentinos");
     }
 
     public void converterReaisParaPesosChilenos(double valorRecebido){
-        double moedaPesoChileno = valorRecebido / 0.0040;
+        double moedaPesoChileno = valorRecebido / 0.0064;
         moedaPesoChileno = (double) Math.round(moedaPesoChileno * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaPesoChileno + " Pesos Chilenos");
     }
--- ConversorParaReais.java
+++ ConversorParaReais.java
@@ -5,30 +5,30 @@ import javax.swing.JOptionPane;
 public class ConversorParaReais {
 
 	public void converterDolaresParaReais(double valorRecebido){
-        double moedaDolar = valorRecebido * 5.13;
+        double moedaDolar = valorRecebido * 5.09;
         moedaDolar = (double) Math.round(moedaDolar * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaDolar + " Reais");
     }
 
     public void converterEurosParaReais(double valorRecebido){
-        double moedaEuro = valorRecebido * 10.85;
+        double moedaEuro = valorRecebido * 5.55;
         moedaEuro = (double) Math.round(moedaEuro * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaEuro + " Reais");
     }
 
     public void converterLibrasParaReais(double valorRecebido){
-        double moedaLibra = valorRecebido * 6.33;
+        double moedaLibra = valorRecebido * 6.31;
         moedaLibra = (double) Math.round(moedaLibra * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + moedaLibra + " Reais");
     }
     public void converterPesosArgentinosParaReais(double valorRecebido){
-        double moedaPesoArgentino = valorRecebido * 0.039;
+        double moedaPesoArgentino = valorRecebido * 0.024;
         moedaPesoArgentino = (double) Math.round(moedaPesoArgentino * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $" + moedaPesoArgentino + " Reais");
     }
 
     public void converterPesosChilenosParaReais(double valorRecebido){
-        double moedaPesoChileno = valorRecebido * 0.0040;
+        double moedaPesoChileno = valorRecebido * 0.0064;
         moedaPesoChileno = (double) Math.round(moedaPesoChileno * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $" + moedaPesoChileno + " Reais");
     }
